323d43f63fe0da21234fde35a7ab701524e08d54
Axel <115540473+axel-roub@users.noreply.github.com>
2023-02-19 14:15:07 +0100

    Add files via upload

---
+++ RPG/src/main/main.java
@@ -0,0 +1,14 @@
+package main;
+import entite.*;
+import carte.*;
+import item.*;
+
+
+
+
+	public class main {
+		public static void main(String[] Args) {
+			map plateau = new map();
+			plateau.showMap();
+		}
+}
