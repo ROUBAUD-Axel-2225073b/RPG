0bf542a709b8460c660d5cfc61ad21b0b919b8a3
Axel <115540473+axel-roub@users.noreply.github.com>
2023-02-27 23:00:17 +0100

    remis fichier + ajout
    ajout des deplacement du hero sur la map

---
+++ src/entite/Entity.java
@@ -0,0 +1,57 @@
+package entite;
+
+public class Entity {
+    private int X;
+    private int Y;
+    private int HP;
+    private int baseAttack;
+    private int baseDefense;
+    private int baseSpeed;
+        
+    public Entity(int x, int y, int hP, int baseAttack, int baseDefense, int baseSpeed) {
+        super();
+        X = x;
+        Y = y;
+        HP = hP;
+        this.baseAttack = baseAttack;
+        this.baseDefense = baseDefense;
+        this.baseSpeed = baseSpeed;
+    }
+    
+    public int getX() {
+        return X;
+    }
+    public void setX(int x) {
+        X = x;
+    }
+    public int getY() {
+        return Y;
+    }
+    public void setY(int y) {
+        Y = y;
+    }
+    public int getHP() {
+        return HP;
+    }
+    public void setHP(int hP) {
+        HP = hP;
+    }
+    public int getBaseAttack() {
+        return baseAttack;
+    }
+    public void setBaseAttack(int baseAttack) {
+        this.baseAttack = baseAttack;
+    }
+    public int getBaseDefense() {
+        return baseDefense;
+    }
+    public void setBaseDefense(int baseDefense) {
+        this.baseDefense = baseDefense;
+    }
+    public int getBaseSpeed() {
+        return baseSpeed;
+    }
+    public void setBaseSpeed(int baseSpeed) {
+        this.baseSpeed = baseSpeed;
+    }
+}    
---
+++ src/item/potion.java
@@ -0,0 +1,17 @@
+package item;
+
+public class potion {
+    private int soin;
+    
+    public potion(int soin) {
+        this.soin = soin;
+    }
+    
+    public int getSoin() {
+        return soin;
+    }
+    
+    public void setSoin(int soin) {
+        this.soin = soin;
+    }
+}
